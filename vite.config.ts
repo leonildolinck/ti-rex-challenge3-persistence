742dea18cb4782fa908f330babb13d1b2e078df1
Leonildo Linck <leonildolinck@gmail.com>
2025-02-14 17:26:45 -0300

    feat(main) allow deploy on vitecfg

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    allowedHosts: [
+      'ec2-34-239-122-225.compute-1.amazonaws.com',
+      'localhost',
+      '127.0.0.1'
+    ]
   },
 })
